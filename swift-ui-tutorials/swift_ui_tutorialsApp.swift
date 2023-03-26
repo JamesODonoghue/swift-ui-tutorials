def7def46be826028de0ad050becef2d69e8bd75
James O'Donoghue <jameso@James-ODonoghue-Macbook-Pro-2.local>
2023-03-25 21:41:24 -0700

    chore: initial commit

---
+++ swift-ui-tutorials/swift_ui_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_tutorialsApp.swift
+//  swift-ui-tutorials
+//
+//  Created by James O'Donoghue on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
